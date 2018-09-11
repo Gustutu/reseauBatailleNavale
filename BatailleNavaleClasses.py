6315000dfbfcc14901b6f082260b948ae694da0c
Gustutu <augustin.turrata@gmail.com>
2018-09-11 16:56:06 +0200

    ajout de la méthode addClient  et addBoat. Ajout de methode __str__ pour la classe boat et client

--- BatailleNavaleClasses.py
+++ BatailleNavaleClasses.py
@@ -1,15 +1,14 @@
 
 import pygame
 
-
+# utilisation de set pour list de bateau par client
 
 
 class Bateau:
-    # Xpos=None
-    # Ypos=None
-    
-    instanceTab=[]
-    def __init__(self, size, _Xpos=None, _Ypos=None, _rotation=None):    
+
+    instanceTab = []
+
+    def __init__(self, size, _Xpos=None, _Ypos=None, _rotation=None):
         self.instanceTab.append(self)
         self.size = size
         self.etat = [1]*size
@@ -22,29 +21,44 @@ class Bateau:
         self.Ypos = _Ypos
         self.rotation = _rotation
 
-    
+    def __str__(self):
+        return "size:{},Xpos:{},Ypos:{},rotation{}".format(self.size,self.Xpos,self.Ypos,self.rotation)
+           
 
 
 class client:
-    # bateau[]=Null
     def __init__(self, name):
         self.name = name
-        self.Boats=None
+        self.Boats = []
+
+    def addBoat(self, boat):
+        self.Boats.append(boat)
+
+    def __str__(self):
+
+        toreturn=""
+        for boat in self.Boats:
+            toreturn+=str(boat)+"\n"
+        return "nom:{} \n {}".format(self.name,toreturn)
 
 
 class BoardGame:
-    # listClient[]
+    listClient = [None]
     # boardTab[][]
+
     def __init__(self, size):
+        # creation d'un tableau 2D de zeros de taille size
         self.boardTab = [[0] * size for _ in range(size)]
+
+    def addClient(self, client):
+        self.listClient.append(client)
 
     def renderBoats(self, bateau):
         if bateau.rotation == 0:
-            for i in range(-1,1):
-                if(self.boardTab[bateau.Ypos][bateau.Xpos]==1):
+            for i in range(-1, 1):
+                if(self.boardTab[bateau.Ypos][bateau.Xpos] == 1):
                     break
 
-            
             i = -1
             for caseEtatBateau in bateau.etat:
                 self.boardTab[bateau.Ypos][bateau.Xpos+i] = caseEtatBateau
@@ -53,11 +67,10 @@ class BoardGame:
         else:
             i = -1
             for caseEtatBateau in bateau.etat:
-                self.boardTab[bateau.Ypos+i][bateau.Xpos]=caseEtatBateau
-                i=i+1
+                self.boardTab[bateau.Ypos+i][bateau.Xpos] = caseEtatBateau
+                i = i+1
 
-
-    def print(self):
+    def print(self):  # affiche le tableau boardTab a la maniere d'un tableau
         for c in self.boardTab:
             print(*c, sep='  ')
 
--- main.py
+++ main.py
@@ -7,20 +7,24 @@ print("coucou")
 
 petitBateau = BatNav.Bateau(3, 2, 2, 1)
 moyenBateau = BatNav.Bateau(4, 2, 2, 1)
-boardGame = BatNav.BoardGame(5)
+boardGame = BatNav.BoardGame(10)
+
+hugo=BatNav.client("Hugo")
+
+hugo.addBoat(petitBateau)
+hugo.addBoat(moyenBateau)
+print(hugo)
 
 
-print(petitBateau.size)
-print(petitBateau.etat)
-print(len(BatNav.Bateau.instanceTab))
+
+
 
 #print ("".join(str(boardGame.boardTab)))
 
 #print ('\n'.join(''.join(*zip(*row)) for row in boardGame.boardTab))
 
-print(BatNav.Bateau.instanceTab)
+
 
 boardGame.renderBoats(petitBateau)
-test = print(boardGame.boardTab)
 boardGame.print()
 time.sleep(5)
