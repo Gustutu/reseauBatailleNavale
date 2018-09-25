423f8d5ee086bfda3a05f9bfe53886de73e473bc
Gustutu <augustin.turrata@gmail.com>
2018-09-25 23:55:24 +0200

    wayyyyyyy cleaner code bug out of range resolved
    fonctionne!

--- BatailleNavaleClasses.py
+++ BatailleNavaleClasses.py
@@ -1,8 +1,10 @@
 
 
 # utilisation de set pour list de bateau par client
-import socket 
+import socket
 import select
+
+
 class Bateau:
 
     instanceTab = []
@@ -21,65 +23,8 @@ class Bateau:
         self.rotation = _rotation
 
     def __str__(self):
-        return "size:{},Xpos:{},Ypos:{},rotation{}".format(self.size,self.Xpos+1,self.Ypos+1,self.rotation)
-           
+        return "size:{},Xpos:{},Ypos:{},rotation{}".format(self.size, self.Xpos+1, self.Ypos+1, self.rotation)
 
-
-"""class socketManager() : #la calsse socket manager hérite du module socket
-
-    import socket 
-    import select
-    hote = ''
-    port = 12800
-
-    connexion_principale = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    connexion_principale.bind((hote, port))
-    connexion_principale.listen(5)
-    print("Le serveur écoute à présent sur le port {}".format(port))
-
-    serveur_lance = True
-    clients_connectes = []
-    while serveur_lance:
-        # On va vérifier que de nouveaux clients ne demandent pas à se connecter
-        # Pour cela, on écoute la connexion_principale en lecture
-        # On attend maximum 50ms
-        connexions_demandees, wlist, xlist = select.select([connexion_principale],
-            [], [], 0.05)
-        
-        for connexion in connexions_demandees:
-            connexion_avec_client, infos_connexion = connexion.accept()
-            # On ajoute le socket connecté à la liste des clients
-            clients_connectes.append(connexion_avec_client)
-        
-        # Maintenant, on écoute la liste des clients connectés
-        # Les clients renvoyés par select sont ceux devant être lus (recv)
-        # On attend là encore 50ms maximum
-        # On enferme l'appel à select.select dans un bloc try
-        # En effet, si la liste de clients connectés est vide, une exception
-        # Peut être levée
-        clients_a_lire = []
-        try:
-            clients_a_lire, wlist, xlist = select.select(clients_connectes,
-                    [], [], 0.05)
-        except select.error:
-            pass
-        else:
-            # On parcourt la liste des clients à lire
-            for client in clients_a_lire:
-                # Client est de type socket
-                msg_recu = client.recv(1024)
-                # Peut planter si le message contient des caractères spéciaux
-                msg_recu = msg_recu.decode()
-                print("Reçu {}".format(msg_recu))
-                client.send(b"5 / 5")
-                if msg_recu == "fin":
-                    serveur_lance = False
-
-    print("Fermeture des connexions")
-    for client in clients_connectes:
-        client.close()
-
-    connexion_principale.close()"""
 
 class gameMaster:
     def __init__(self, name):
@@ -87,81 +32,84 @@ class gameMaster:
         self.Boats = []
 
     def tryAddBoat(self, boat):
-        if BoardGame.tryAndAddBoat(boat)==1:
+        if BoardGame.tryAndAddBoat(boat) == 1:
             self.Boats.append(boat)
-
 
     def __str__(self):
 
-        toreturn=""
+        toreturn = ""
         for boat in self.Boats:
-            toreturn+=str(boat)+"\n"
-        return "nom:{} \n {}".format(self.name,toreturn)
+            toreturn += str(boat)+"\n"
+        return "nom:{} \n {}".format(self.name, toreturn)
 
 
+# class client :
 
-#class client :
-    
 
 class BoardGame:
     listClient = [None]
-    size=0
-    # boardTab[][]
+    size = 0
+    boardTab = [0][0]
+
     def __init__(self, size):
         # creation d'un tableau 2D de zeros de taille size
-        self.boardTab = [[0] * size for _ in range(size)]
-        self.size=size
+        BoardGame.boardTab = [[0] * size for _ in range(size)]
+        BoardGame.size = size
 
     def addClient(self, client):
         self.listClient.append(client)
 
-    def renderAllBoats(self,BoatList):
-        for boat in BoatList:
-            self.renderBoats(boat)
-
-
+  #  def renderAllBoats(self, BoatList):
+   #     for boat in BoatList:
+    #        self.renderBoats(boat)
 
     def tryAndAddBoat(bateau):
-        #horizontal
+        # horizontal
         if bateau.rotation == 0:
-            #Verification collision
+            # Verification collision
             if bateau.Xpos+bateau.size > BoardGame.size:
                 print("out of range")
                 return 0
 
             for i in range(0, bateau.size-1):
-                if self.boardTab[bateau.Ypos][bateau.Xpos+i] == 1:
-                    print("there is another boat here (Pos: X Y)", bateau.Xpos+1, bateau.Ypos+1)
+                if BoardGame.boardTab[bateau.Ypos][bateau.Xpos+i] == 1:
+                    print("there is another boat here (Pos: X Y)",
+                          bateau.Xpos+1, bateau.Ypos+1)
                     return 0
 
-            #i = 0
+            # i = 0
             # Mise des cases à 1
                 else:
-                        for caseEtatBateau in bateau.etat:
-                            self.boardTab[bateau.Ypos][bateau.Xpos+i] = caseEtatBateau
-                            i = i+1
-                        return 1
-        #vertical
+                    for caseEtatBateau in bateau.etat:
+                        BoardGame.boardTab[bateau.Ypos][bateau.Xpos +
+                                                        i] = caseEtatBateau
+                        i = i+1
+                    return 1
+        # vertical
         else:
-            #Verification collision
+            # Verification collision
             if bateau.Ypos+bateau.size > BoardGame.size:
+                print("max boat is ", bateau.Ypos+bateau.size)
+                print("boardSize is ", BoardGame.size)
                 print("out of range")
                 return 0
 
             for i in range(0, bateau.size-1):
-                if self.boardTab[bateau.Ypos+i][bateau.Xpos] == 1:
-                    print("there is another boat here (Pos: X Y)", bateau.Xpos, bateau.Ypos)
+                if BoardGame.boardTab[bateau.Ypos+i][bateau.Xpos] == 1:
+                    print("there is another boat here (Pos: X Y)",
+                          bateau.Xpos, bateau.Ypos)
                     return 0
-                #i = 0
+                # i = 0
             # Mise des cases à 1
                 else:
                     for caseEtatBateau in bateau.etat:
-                        self.boardTab[bateau.Ypos+i][bateau.Xpos] = caseEtatBateau
+                        BoardGame.boardTab[bateau.Ypos +
+                                           i][bateau.Xpos] = caseEtatBateau
                         i = i+1
                     return 1
 
     def print(self):  # affiche le tableau boardTab a la maniere d'un tableau
-        for c in self.boardTab:
+        for c in BoardGame.boardTab:
             print(*c, sep='  ')
 
     # def addClient(self, client):
--- main.py
+++ main.py
@@ -5,37 +5,31 @@ SIZE: int = 10
 
 #petitBateau = BatNav.Bateau(3, 6, 6, 0)
 #moyenBateau = BatNav.Bateau(4, 2, 2, 1)
-#grandBateau = BatNav.Bateau(5, 2, 7, 0)
-boardGame = BatNav.BoardGame(SIZE)
+# grandBateau = BatNav.Bateau(5, 2, 7, 0)
+boardGame = BatNav.BoardGame(10)
 
-#hugo=BatNav.client("Hugo")
-
-
+# hugo=BatNav.client("Hugo")
 
 
-name=input("Quel est ton nom? ")
-gameMaster=BatNav.gameMaster(name)
-again='ok'
-i=0
+name = input("Quel est ton nom? ")
+gameMaster = BatNav.gameMaster(name)
+again = 'ok'
+i = 0
 
 while again == 'ok':
     taille = input("Quelle taille fait votre bateau? ")
     posX = input("Quelle coordonnée X ? ")
     posY = input("Quelle coordonnée Y ? ")
     orientation = input("Quelle orientation (horizontal=0/vertical=1)? ")
-    gameMaster.tryAddBoat(BatNav.Bateau(int(taille), int(posX), int(posY), int(orientation)))
+    gameMaster.tryAddBoat(BatNav.Bateau(
+        int(taille), int(posX), int(posY), int(orientation)))
     again = input("Voulez vous faire un autre bateau (ok/no) ? ")
-    i=i+1
+    i = i+1
 
 
 print(gameMaster)
 
-boardGame.renderAllBoats(gameMaster.Boats)
-
-#for x in range(0, len(bateaux)):
- #   boardGame.renderBoats(bateaux[x], SIZE)
-
-#oardGame.print()
+boardGame.print()
 
 time.sleep(5)
 print(".")
@@ -57,7 +51,3 @@ print("C'est désormais le moment pour les joueurs de participer !")
 #print ("".join(str(boardGame.boardTab)))
 
 #print ('\n'.join(''.join(*zip(*row)) for row in boardGame.boardTab))
-
-
-
-
